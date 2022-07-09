7d016f6264ec089442c6c67a3e1b932994676c06
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-07-09 22:08:21 +0300

    Fix according to code review

--- homework3/homework3.py
+++ homework3/homework3.py
@@ -1,4 +1,5 @@
 import csv
+
 import requests
 
 from faker import Faker
@@ -46,18 +47,21 @@ def error_handler(error):
     location="query"
 )
 def generate_students(count):
-    with open('students.csv', 'w') as my_csv:
-        for i in range(count):
-            my_csv.write(
-                fake.name() + ',' + fake.profile('mail')['mail'] + ',' + fake.password(length=8) + ',' + str(
-                    fake.date_of_birth(minimum_age=18, maximum_age=45)) + '\n')
-    with open('students.csv', 'r') as my_json:
-        result = csv.reader(my_json)
-        final = {'students': []}
-        for row in result:
-            final['students'].append(({'Name': row[0], 'Email': row[1], 'Password': row[2], 'Birthday': row[3]}))
+    fieldnames = ['Name', 'Email', 'Password', 'Birthday']
+    students = [
+        {
+            'Name': fake.name(),
+            'Email': fake.profile('mail')['mail'],
+            'Password': fake.password(length=8),
+            'Birthday': str(fake.date_of_birth(minimum_age=18, maximum_age=45))
+        } for _ in range(count)
+    ]
+    with open('students.csv', 'w', newline='',) as my_csv:
+        writer = csv.DictWriter(my_csv, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(student for student in students)
     app.config.update(JSON_SORT_KEYS=False)
-    return final
+    return jsonify(students)
 
 
 @app.route("/bitcoin")
@@ -67,37 +71,32 @@ def generate_students(count):
             load_default='USD',
         ),
         'count': fields.Int(
-            load_default=1,
-            validate=[validate.Range(min=1)]
+            load_default=None,
+            validate=[validate.Range(min=1, max=1000000)]
         )
     },
     location="query"
 )
 def get_bitcoin_value(currency, count):
     currency = currency.upper()
-    url = 'https://bitpay.com/api/rates'
-    url_for_symbols = 'https://bitpay.com/currencies'
+    url = requests.get('https://bitpay.com/api/rates/').json()
+    url_for_symbols = requests.get('https://bitpay.com/currencies').json()
 
-    result = requests.get(url, {})
-    result = result.json()
-    available_currencies = [x['code'] for x in result]
+    available_currencies = [x['code'] for x in url]
     if currency not in available_currencies:
         abort(400, messages='Not a valid currency!')
 
-    result_for_symbols = requests.get(url_for_symbols, {})
-    result_for_symbols = result_for_symbols.json()
-
-    for row in result:
+    for row in url:
         if currency == row['code']:
             currency_rate = int(row['rate'])
-    for row in result_for_symbols['data']:
+    for row in url_for_symbols['data']:
         if currency == row['code']:
             currency_symbol = row['symbol']
-    if count != 1:
-        return f'<center><p>You can buy {round((count / currency_rate), 5)} bitcoins for {count} {currency_symbol}</p></center>'
-    else:
+    if count is None:
         return f'<center><p>The price of one bitcoin is {currency_rate} {currency_symbol}</p></center>'
+    else:
+        return f'<center><p>You can buy {round((count / currency_rate), 5)} bitcoins for {count} {currency_symbol}</p></center>'
 
-    
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
